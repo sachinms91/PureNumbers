f9c14927ad829c448e79ae73e6b3d7eecb548421
Sachin M S <Sachin.MS@kpmg.co.uk>
2019-06-24 14:18:08 +0530

    Source code commit

---
+++ PureNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String line;
+
+            line = Console.ReadLine();
+            int T = Convert.ToInt32(line);
+
+            for (int t_i = 0; t_i < T; t_i++)
+            {
+                line = Console.ReadLine();
+                int N = Convert.ToInt32(line);
+
+                String out_ = Solve(N);
+                Console.Out.WriteLine(out_);
+
+            }
+            Console.ReadLine();
+        }
+
+        static String Solve(int N)
+        {
+            List<int> list = new List<int> ();
+
+            for (int k = 44; k<1000000;k++)
+            {
+                string kstring = k.ToString();
+                if ((kstring.Contains("4") || kstring.Contains("5")) && kstring.Contains("1") && kstring.Contains("2") && kstring.Contains("3") && kstring.Contains("6") && kstring.Contains("7") && kstring.Contains("8") && kstring.Contains("9") && kstring.Contains("0"))
+
+                { 
+                    if(k.ToString().Length %2 ==0)
+                    {
+
+                        int m  = k;
+                        int temp = m;
+                        int r;
+                        int sum = 0;
+                        while (m > 0)
+                        {
+                            r = m % 10;
+                            sum = (sum * 10) + r;
+                            m = m / 10;
+                        }
+
+                        if(temp == sum)
+                        {
+                            list.Add(k);
+                        }
+
+                    }
+
+                }
+           
+                }
+            return list.ElementAt(N-1).ToString();
+            }
+        }
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # PureNumbers
+
+You are given a special set of pure numbers.
+
+A pure number has the following properties:
+
+It comprises only 4 and 5.
+It is a palindrome number.
+The number of digits in a pure number is even.
+For example, 4444,5445,44 are the pure numbers while 12,444,4545 are not. You are given a list of pure numbers in an ascending order. So, purenumber[1] < purenumber[2].
+
+The first few elements in the list are as follows: 44,55,4444,....
+
+You will be given an integer and you have to find pure number from the list.
+
+Input format
+
+First line: denoting the number of test cases
+Next lines: A number 
+
+Output format
+
+Print the pure number.
+
+Constraints
+
+
+Sample Input
+3
+1
+2
+3
+Sample Output
+44
+55
+4444
+Explanation
+Here, T=3 that means there are 3 test cases
+In the 1st test case N=1, so we have to print the 1st pure number that is 44
+Similarly, in 2nd and 3rd Test cases, we have to print the 2nd and 3rd pure number in the series. Hence, we have to print 55 and 4444 respectively.
